5811c154cea147161d5e5a83e52a2dc7cb92e5a6
unknown <ali.jalilvand2203@gmail.com>
2024-07-02 13:06:56 +0330

    AP

---
+++ src/HttpHandler/CommentHandler.java
@@ -0,0 +1,73 @@
+package HttpHandler;
+
+import Controllers.CommentController;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONObject;
+import utils.ExtractUserAuth;
+
+import java.io.*;
+import java.sql.SQLException;
+
+public class CommentHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        CommentController commentController = null;
+        try {
+            commentController = new CommentController();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        String response = "";
+        String[] splittedPath = path.split("/");
+        switch (method) {
+            case "GET": //port:id/direct/person1/person2
+                if (!splittedPath[2].equals(ExtractUserAuth.extract(exchange))) {
+                    response = "permission-denied";
+                    break;
+                }
+//                if (splittedPath.length == 3) { //port:id/direct/person
+//                    try {
+//                        response = commentController.getNotify(splittedPath[2], 20);
+//                    } catch (SQLException e) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+//                }
+                try {
+                    response = commentController.getComments(splittedPath[2]);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "POST": // ip:port/direct + body
+                InputStream requestBody = exchange.getRequestBody();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(requestBody));
+                StringBuilder body = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    body.append(line);
+                }
+                requestBody.close();
+                String newMessage = body.toString();
+                JSONObject jsonObject = new JSONObject(newMessage);
+                if (!jsonObject.getString("sender").equals(ExtractUserAuth.extract(exchange))) {
+                    response = "permission-denied";
+                    break;
+                }
+                response = "Done!";
+                try {
+                    commentController.addComment( jsonObject.getString("sender"), jsonObject.getString("text"));
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
---
+++ src/HttpHandler/MediaHandler.java
@@ -0,0 +1,68 @@
+package HttpHandler;
+
+import Controllers.UserController;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import utils.ExtractUserAuth;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.sql.SQLException;
+import java.util.Collections;
+
+public class MediaHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        UserController userController = null;
+        try {
+            userController = new UserController();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String response = "";
+        String method = exchange.getRequestMethod();
+        String path = exchange.getRequestURI().getPath();
+        String[] splittedPath = path.split("/");
+        // ip:port/media/userID/mediaName/mediaType
+        if (splittedPath.length != 5) {
+            response = "unknown-request";
+        } else switch (method) {
+            case "GET":
+                File file;
+                file = new File("./Assets/" + splittedPath[2] + "/" + splittedPath[3] + "." + splittedPath[4]);
+                if (!file.exists()) {
+                    response = "no file";
+                    break;
+                }
+                exchange.getResponseHeaders().put("Content-Type", Collections.singletonList(splittedPath[4]));
+                exchange.sendResponseHeaders(200, file.length());
+                OutputStream outputStream = exchange.getResponseBody();
+                Files.copy(file.toPath(), outputStream);
+                outputStream.close();
+                return;
+            case "POST":
+                if (!userController.isUserExists(splittedPath[2])) {
+                    response = "user-not-found";
+                    break;
+                }
+                if (!splittedPath[2].equals(ExtractUserAuth.extract(exchange))) {
+                    response = "permission-denied";
+                    break;
+                }
+                Files.copy(exchange.getRequestBody(), Path.of("./Assets/" , splittedPath[2], splittedPath[3] + "." + splittedPath[4]), StandardCopyOption.REPLACE_EXISTING);
+                exchange.getRequestBody().close();
+                response = "done";
+                break;
+            default:
+                response = "unknown-request2";
+                break;
+        }
+
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
+}
